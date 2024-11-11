067dc62ac89861aebbcb8914efddec0a16d9f7f4
Sreeram1503 <136753624+Sreeram1503@users.noreply.github.com>
2024-11-11 07:52:44 -0500

    Add files via upload

---
+++ Theft_detection_model/generate_encoding.py
@@ -0,0 +1,20 @@
+import face_recognition
+import pickle
+
+# Loading image of my face 
+image = face_recognition.load_image_file("face1.jpg")
+
+# Generate the face encoding for the image
+face_encodings = face_recognition.face_encodings(image)
+
+# Checking if an encoding was found
+if len(face_encodings) > 0:
+    your_encoding = face_encodings[0]
+    print("Face encoding generated successfully!")
+
+    # Save the encoding to a file
+    with open("face_encoding.pkl", "wb") as file:
+        pickle.dump(your_encoding, file)
+    print("Face encoding saved as 'your_face_encoding.pkl'")
+else:
+    print("No face detected in the image. Please use a clearer image.")
